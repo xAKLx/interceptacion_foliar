73503be2d5be2ba222a0bbd8540ca782273b6dde
Angel Guzman <xaklx@hotmail.com>
2017-12-02 12:38:04 -0400

    d

--- funciones.r
+++ funciones.r
@@ -1,5 +1,36 @@
+source('~/Development/interceptacion_foliar/parser.r')
 setClass("data", slots=list(plantones="list"))
 
+
+#' Crea o actualiza un objeto con la informacion suministrada.
+#' 
+#' Todos los parametros a excepcion de data son frames representativas de los 
+#' datos en los cuales se encuentran la información de dichos parametros. Estos
+#' se consiguen usando las funciones de filtraje.
+#' 
+#' @param plantones información de los plantones.
+#' @param puntosCaptaciones información de los puntos de captaciones
+#' @param sesion información de la sesion
+#' @param eventos información de los eventos
+#' @param data debe ser un objecto creado anteriormente por esta función
+#' o por importarDesdeArchivos. de ser proveido, se tomara este objeto como base y 
+#' actualizara sus datos con la informacion proveida en los demas parametros.
+#' 
+#' @return Un objeto con solo la data proveida en los demas parametros si
+#' el parametro data no es proveido. de otro modo, un objeto con la
+#' informacion de data y con la informacion proveida en los demas parametros.
+#' @export
+importar <- function(plantones, puntosCaptaciones, sesion, eventos, data) {
+  data <- new("data");
+  data@plantones <- read.csv(file = plantones, header = TRUE, sep = ",", stringsAsFactors = FALSE);
+  
+  
+  for(column in colnames(data@plantones)){
+    data@plantones[,column] <- plantonesParser(column, data@plantones[,column])
+  }
+  
+  data
+}
 
 #' Crea o actualiza un objeto con la informacion suministrada.
 #' 
@@ -18,7 +49,7 @@ setClass("data", slots=list(plantones="list"))
 #' el parametro data no es proveido. de otro modo, un objeto con la
 #' informacion de data y con la informacion proveida en los demas parametros.
 #' @export
-importar <- function(plantones, puntosCaptaciones, sesion, eventos, data) {
+importarDesdeArchivos <- function(plantones, puntosCaptaciones, sesion, eventos, data) {
   data <- new("data");
   data@plantones <- read.csv(file = plantones, header = TRUE, sep = ",", stringsAsFactors = FALSE);
   
@@ -39,21 +70,39 @@ importar <- function(plantones, puntosCaptaciones, sesion, eventos, data) {
 #' @param regla una funcion que sera aplicada a cada fila del conjunto de datos
 #' y que debe retornar un logical que defina si la fila sera incluida en el
 #' archivo destino. Esta funcion recibira una lista con la fila a evaluar.
-#' @param destino nombre del archivo destino.
+#' @param filtrador una funcion que sera aplicada a cada fila cullo resultado
+#' de la aplicacion de la funcion regla sea verdadera. De ser proveeida, esta 
+#' funcion recibira una fila y retornara la fila modificada.
+#' @param destino nombre del archivo destino. De ser proveeido, el resultado
+#' de esta funcion se guardara en el archivo especificado.
+#' @return frame del conjunto de datos especificado despues de realizar el
+#' filtraje.
 #' @export
-filtrarData <- function(data, tipoDeData, regla, destino) {
+filtrarData <- function(data, tipoDeData, regla, filtrador, destino) {
   conjunto <- attr(data, tipoDeData);
   
   if(nrow(conjunto) > 0) {
     name  <- conjunto[0,];
     for(index in seq(1:nrow(conjunto))){
-      if(regla(conjunto[index, ]))
-        name[nrow(name)+1,] <- conjunto[index,]; 
+      if(regla(conjunto[index, ])) {
+        if(!missing(filtrador)){
+          name[nrow(name)+1,] <- filtrador(conjunto[index,]); 
+        } else {
+          name[nrow(name)+1,] <- conjunto[index,]; 
+        }
+      }
     }
     
     name
   }
   
+}
+
+filtradorSustituir <- function(columna, nuevoValor) {
+  function(fila) {
+    fila[1,columna] = nuevoValor
+    fila
+  }
 }
   
 #' Evalúa si la columna de fila suministrada es igual al valor
